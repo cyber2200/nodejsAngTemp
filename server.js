44887f433f78a61a079e3bde63c49497941223a8
Ran Argaman <ranware2200@yahoo.com>
2015-12-11 11:00:14 +0000

    Drier..

--- server.js
+++ server.js
@@ -10,119 +10,67 @@ function handleRequest(request, response){
 	}); 
 	console.log(request.url);
 	var staticFolder = '/static';
-	if (request.url.slice(0, staticFolder.length) == staticFolder) {
+	if (request.url.slice(0, staticFolder.length) == staticFolder) { // Handling Static content
 		var contentType = frontendInst.getContentType(request.url);
 		var contentBody = frontendInst.getContentBody(request.url, contentType);
 		response.writeHeader(200, {"Content-Type": contentType});  
 		response.write(contentBody);  
 		response.end(); 
 	} else {
-		if (request.url == '/favicon.ico') {
+		if (request.url == '/favicon.ico') { // Favicon workaround, @ToDo
 			response.write('');  
 			response.end(); 						
-		} else if (request.headers.accept.indexOf('application/json') != -1) {
+		} else if (request.headers.accept.indexOf('application/json') != -1) { // Handling API calls
 			if (request.url.indexOf('/api') == 0) {
 				var action = request.url.split('/').pop();
 				response.writeHeader(200, {"Content-Type": "application/json"});
-				switch (action) {
-					case 'getData':
-						var data = apiInst.getData();
-						response.write(JSON.stringify(data));  
-						response.end(); 
-					break;
-					case 'getUser':
-						var payload = '';
-						request.addListener('data', function(chunk){
-							payload += chunk;
-						});
-	
-						request.addListener('error', function(error){
-							next(err);
-						});
+				var payload = '';
+				request.addListener('data', function(chunk){
+					payload += chunk;
+				});
+				/*
+				request.addListener('error', function(error){
+					next(err);
+				});
+				*/
+				request.addListener('end', function(chunk){
+					if (chunk) {
+						payload += chunk;
+					}
 
-						request.addListener('end', function(chunk){
-							if (chunk) {
-								payload += chunk;
-							}
-							payloadObj = JSON.parse(payload);
+					switch (action) { // Routing...
+						case 'getUser':
+							var payloadObj = JSON.parse(payload);
 							var data = apiInst.getUser(payloadObj.id);
-							response.write(JSON.stringify(data));  
-							response.end(); 
-						});
-					break;
-					case 'updateUser':
-						var payload = '';
-						request.addListener('data', function(chunk){
-							payload += chunk;
-						});
-	
-						request.addListener('error', function(error){
-							next(err);
-						});
-
-						request.addListener('end', function(chunk){
-							if (chunk) {
-								payload += chunk;
-							}
-							payloadObj = JSON.parse(payload);
+						break;
+						case 'updateUser':
+							var payloadObj = JSON.parse(payload);
 							var data = apiInst.updateUser(payloadObj);
-							response.write(JSON.stringify(data));  
-							response.end(); 
-						});					
-					break;
-					case 'addUser':
-						var payload = '';
-						request.addListener('data', function(chunk){
-							payload += chunk;
-						});
-	
-						request.addListener('error', function(error){
-							next(err);
-						});
-
-						request.addListener('end', function(chunk){
-							if (chunk) {
-								payload += chunk;
-							}
-							payloadObj = JSON.parse(payload);
+						break;
+						case 'addUser':
+							var payloadObj = JSON.parse(payload);
 							var data = apiInst.addUser(payloadObj);
-							response.write(JSON.stringify(data));  
-							response.end(); 
-						});					
-					break;
-
-					case 'deleteUser':
-						var payload = '';
-						request.addListener('data', function(chunk){
-							payload += chunk;
-						});
-	
-						request.addListener('error', function(error){
-							next(err);
-						});
-
-						request.addListener('end', function(chunk){
-							if (chunk) {
-								payload += chunk;
-							}
-							payloadObj = JSON.parse(payload);
+						break;
+						case 'deleteUser':
+							var payloadObj = JSON.parse(payload);
 							var data = apiInst.deleteUser(payloadObj);
-							response.write(JSON.stringify(data));  
-							response.end(); 
-						});					
-					break;
-					default:
-						var data = {'Error' : 'No action defined'};
-						response.write(JSON.stringify(data));  
-						response.end(); 
-				}										
+						break;
+						case 'getData':
+							var data = apiInst.getData();
+						break;
+						default:
+							var data = {'Error' : 'No action defined'};
+					}
+					response.write(JSON.stringify(data));  
+					response.end(); 
+				});
 			} else {
 				response.writeHeader(200, {"Content-Type": "application/json"});
 				var data = {'error' : 'error'};
 				response.write(JSON.stringify(data));  
 				response.end(); 									
 			}
-		} else {
+		} else { // Handling the standard pages using layout and templates
 			var layout = frontendInst.getLayout();
 			var template = 'main';
 			var content = frontendInst.getContentFromTemplate(template);
